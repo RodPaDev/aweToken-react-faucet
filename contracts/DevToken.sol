2b72b39c8ccea89d7fff66972bebfdc64d050d0c
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-11-14 18:37:25 +0000

    create erc-20 smart contract

---
+++ contracts/DevToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// SPDX-License-Identifier: MIT
+
+contract DevToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 10000 * (10**18));
+    }
+
+    function faucet(address recipient, uint256 amount) external {
+        _mint(recipient, amount);
+    }
+}
