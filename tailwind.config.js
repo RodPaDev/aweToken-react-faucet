7cb29e912a5bc164a052ccfb0bf43ed7668d774f
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2021-11-15 23:52:01 +0000

    extend colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#06c19d'
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
